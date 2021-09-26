4d764b643455009cc54d28f0a6a519418576510e
AzizAlameer <zoozz5747@gmail.com>
2021-09-26 22:08:34 +0300

    first commit

--- models.py
+++ models.py
@@ -1,1 +1,75 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import Column, String, Integer, DateTime
 
+database_path = os.environ.get('DATABASE_URL')
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Actor(db.Model):
+  __tablename__ = 'Actor'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String, nullable=False)
+  age = Column(Integer, nullable=False)
+  gender = Column(String, nullable=False)
+
+  def get_name(self):
+    return self.name
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+  
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'age': self.age,
+      'gender': self.gender
+    }
+
+
+class Movie(db.Model):
+  __tablename__ = 'Movie'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String, nullable=False)
+  release_date = Column(DateTime(), default=datetime.utcnow)
+
+  def get_title(self):
+    return self.title
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date
+    }
--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+export DATABASE_URL='postgresql://postgres:1366@localhost:5432/test'
+export ENV='development'
+export FLASK_APP=app
+export FLASK_DEBUG=True
